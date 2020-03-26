ce3e6e66af426a536c532fb7be26b27565a5f3bc
NullPointExcption <625160751@qq.com>
2020-03-26 16:02:55 +0800

    ceshi6

--- src/main/java/com/githubtest/Controller/controllerTest.java
+++ src/main/java/com/githubtest/Controller/controllerTest.java
@@ -2,11 +2,13 @@ package com.githubtest.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class controllerTest {
 
     @RequestMapping("/get")
+    @ResponseBody
     public String tes(){
         return "自动部署完成";
     }
