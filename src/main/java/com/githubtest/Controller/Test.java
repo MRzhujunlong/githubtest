fdf41a1f21d9886445846d2dcf9115a9d6f69530
NullPointExcption <625160751@qq.com>
2020-03-26 14:35:27 +0800

    ceshi1

--- src/main/java/com/githubtest/Controller/Test.java
+++ src/main/java/com/githubtest/Controller/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("输出文字");
     }
+
+    public static void te(){
+        System.out.println("88888888888888");
+    }
 }
