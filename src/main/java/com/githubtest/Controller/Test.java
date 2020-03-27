0238ad9c5c87da7262329dd96e082ab861c3665e
NullPointExcption <625160751@qq.com>
2020-03-27 09:41:28 +0800

    ceshi7

--- src/main/java/com/githubtest/Controller/Test.java
+++ src/main/java/com/githubtest/Controller/Test.java
@@ -1,10 +1,14 @@
 package com.githubtest.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/test")
 public class Test {
     public static void main(String[] args) {
         System.out.println("输出文字");
     }
 
+    @RequestMapping("/te")
     public static void te(){
         System.out.println("88888888888888");
     }
