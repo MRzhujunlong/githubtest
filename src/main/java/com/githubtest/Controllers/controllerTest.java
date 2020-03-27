45f4cef2a6b5cc72907f9dfe679016a3c60f1d55
NullPointExcption <625160751@qq.com>
2020-03-27 11:42:28 +0800

    ceshi9

--- src/main/java/com/githubtest/Controllers/controllerTest.java
+++ src/main/java/com/githubtest/Controllers/controllerTest.java
@@ -1,4 +1,4 @@
-package com.githubtest.Controller;
+package com.githubtest.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
